f91a1d553a114dcd8ad1449af8ac69e3a9de9a67
dibyagopal <dibyasau@gmail.com>
2020-12-24 12:54:58 +0530

    New html update

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,6 +8,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { Helmet } from "react-helmet";
 
 import Header from "./includes/siteHeader"
 import Footer from "./includes/siteFooter"
@@ -26,13 +27,19 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <div className="outerWrapper">
-      <div className="mainContainer">
-        <Header />
-        {children}
+    <React.Fragment>
+      <Helmet>
+      <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet" />
+      </Helmet>
+      <div className="outerWrapper">
+        <div className="mainContainer">
+          <Header />
+          {children}
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </React.Fragment>
+
   )
 }
 
