3aa1dd182037e677a9fe236f75afb6e5a888a703
dibyagopal <dibyasau@gmail.com>
2020-12-25 20:03:01 +0530

    New html update

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
-        short_name: `starter`,
+        short_name: `DA`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
