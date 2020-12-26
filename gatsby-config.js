95cd816b6df859102a5a8876800d6192effac2e2
dibyagopal <dibyasau@gmail.com>
2020-12-26 17:41:23 +0530

    Add project page

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Digital Avenues`,
         short_name: `DA`,
         start_url: `/`,
         background_color: `#663399`,
